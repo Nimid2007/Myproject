fb5d27109845899eadef58fd5b0e18825b81586b
mnimi <mnimid@mail.ru>
2023-08-06 14:08:00 +0300

    dz8

--- WebAdv/app_advertisements/models.py
+++ WebAdv/app_advertisements/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
-class Advertisements(models.Model):
-    title=models.CharField("заголовок",max_lenght=128)
-    discription=models.TextField("описание")
+# Create your models here.
+class Advertisement(models.Model):
+    title=models.CharField("заголовок",max_length=128)
+    description=models.TextField("описание")
     price=models.DecimalField("цена",max_digits=10,decimal_places=2)
-    auction=models.BooleanField("торг",help_text="отметьте,если торг уместен.")
+    auction=models.BooleanField("торг",help_text="отметьте,если торг уместен")
     created_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
-    
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Advertisement(id={self.id},title={self.title},price={self.price})"
+    class Meta:
+        db_table="advertisements"

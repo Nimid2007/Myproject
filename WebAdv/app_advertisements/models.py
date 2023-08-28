c4a23e0379f1d648f4203383acf93393f2a3ac8f
mnimi <mnimid@mail.ru>
2023-08-28 18:25:59 +0300

    dz9

--- WebAdv/app_advertisements/models.py
+++ WebAdv/app_advertisements/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
+User=get_user_model()
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length=128)
     description = models.TextField("описание")
@@ -10,8 +12,16 @@ class Advertisement(models.Model):
     auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
+    user=models.ForeignKey(User,verbose_name='пользователь',on_delete=models.CASCADE)
+    image=models.ImageField('изобажение',upload_to='advertisements')
+    
+    
+    @admin.display(description='фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html('<img src="{url}" style"max-width:80px ;max-height:80px">',url=self.image.url)
+    
+    
     @admin.display(description='заголовок')
     def blue_title(self):
        return format_html('<span style="color: blue; font-weight: bold;">{}</span>',self.title)  
